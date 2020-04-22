154279c1586e69f10a3a15d5a70f7ad2a540cc15
yifanpu001 <egoisi1948>
2020-04-22 16:23:47 +0800

    add inner test

--- deformable_kernels/modules/cond_conv.py
+++ deformable_kernels/modules/cond_conv.py
@@ -86,3 +86,11 @@ class CondConv2d(nn.Module):
         if self.groups != 1:
             s += ', groups={groups}'
         return s.format(**self.__dict__)
+
+if __name__ == '__main__':
+    input_tensor = torch.rand(2, 4, 224, 224)
+    conv_layer = CondConv2d(num_experts=6, in_channels=4, out_channels=8, kernel_size=3,
+                 stride=1, padding=0, dilation=1, groups=1, bias=False,
+                 padding_mode='zeros')
+    output = conv_layer(input_tensor)
+    print("end")
