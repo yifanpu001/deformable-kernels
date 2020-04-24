fc7d71d03b5972c8465337ba46740b98c3c80c68
yifanpu001 <egoisi1948>
2020-04-25 00:37:21 +0800

    debug of original ver; add comments (shape)

--- deformable_kernels/modules/cond_conv.py
+++ deformable_kernels/modules/cond_conv.py
@@ -26,11 +26,12 @@ class CondConv2d(nn.Module):
         self.stride = stride
         self.dilation = dilation
         self.groups = groups
+        self.padding = padding
         self.padding_mode = padding_mode
         assert not bias
 
         self.weight = nn.Parameter(
-            torch.tensor(
+            torch.Tensor(
                 num_experts * out_channels,
                 in_channels // self.groups,
                 kernel_size,
@@ -43,34 +44,34 @@ class CondConv2d(nn.Module):
     @amp.float_function
     def dynaic_inference(self, x, weight):
         # TODO(Hang Gao @ 12/26): make sure passing weight to amp is necessary.
-        n = x.shape[0]
+        n = x.shape[0]  # n = batch_size
 
-        avg_x = x.mean((2, 3))
-        gate_x = torch.sigmoid(self.fc(avg_x))
+        avg_x = x.mean((2, 3))  # avg_x.shape = [batch_size, Cin]
+        gate_x = torch.sigmoid(self.fc(avg_x))  # gate_x.shape = [batch_size, num_experts]
 
         weight = torch.mm(
-            gate_x,
-            self.weight.reshape(self.num_experts, -1)
-        ).reshape(
-            n * self.out_channels,
+            gate_x,                                     # shape = [batch_size, num_experts]
+            self.weight.reshape(self.num_experts, -1)   # shape = [num_experts, Cout x (Cin // groups) x k x k]
+        ).reshape(                                      # result = [batch_size, Cout x (Cin // groups) x k x k]
+            n * self.out_channels,                      
             self.in_channels // self.groups,
             self.kernel_size,
             self.kernel_size,
         )
-        return weight
+        return weight  # shape = [batch_size x Cout, Cin // groups, k, k]
 
     def forward(self, x):
-        n, _, h, w = x.shape
-        weight = self.dynaic_inference(x, self.weight)
+        n, _, h, w = x.shape  # n = batch_size
+        weight = self.dynaic_inference(x, self.weight)  # shape = [batch_size x Cout, Cin // groups, k, k]
 
         out = nn.functional.conv2d(
-            x.reshape(1, n * self.in_channels, h, w),
-            weight,
+            x.reshape(1, n * self.in_channels, h, w),   # shape = [1, batch_size x Cin, h, w]
+            weight,                                     # shape = [batch_size x Cout, Cin // groups, k, k]
             stride=self.stride,
             padding=self.padding,
             dilation=self.dilation,
             groups=n*self.groups,
-            padding_mode=self.padding_mode,
+            # padding_mode=self.padding_mode,  # torch.nn.functional.conv2d does not have a padding_mode argument
         )
         out = out.reshape(n, self.out_channels, *out.shape[2:])
         return out
@@ -87,10 +88,10 @@ class CondConv2d(nn.Module):
             s += ', groups={groups}'
         return s.format(**self.__dict__)
 
-if __name__ == '__main__':
-    input_tensor = torch.rand(2, 4, 224, 224)
-    conv_layer = CondConv2d(num_experts=6, in_channels=4, out_channels=8, kernel_size=3,
-                 stride=1, padding=0, dilation=1, groups=1, bias=False,
-                 padding_mode='zeros')
-    output = conv_layer(input_tensor)
-    print("end")
+if __name__ == '__main__':
+    input_tensor = torch.rand(2, 3, 224, 224)
+    conv_layer = CondConv2d(num_experts=4, in_channels=3, out_channels=6, kernel_size=3, 
+                 stride=1, padding=0, dilation=1, groups=1, bias=False,
+                 padding_mode='zeros')
+    output = conv_layer(input_tensor)
+    print("end")
